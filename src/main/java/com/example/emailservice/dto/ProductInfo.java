fae8ee5216e31ffd67ea918df44de7382910b43f
Ditt github-maryam12370 <Din github-maryamselemankhil786@gmail.com>
2026-06-02 11:17:45 +0200

    lagged ProductInfo

---
+++ src/main/java/com/example/emailservice/dto/ProductInfo.java
@@ -0,0 +1,11 @@
+package com.example.emailservice.dto;
+
+import java.math.BigDecimal;
+
+public class ProductInfo {
+    Long id ;
+    String name;
+    String description;
+    BigDecimal price;
+    int quantity;
+}
